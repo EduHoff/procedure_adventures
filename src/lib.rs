ab3b905c3c2577ee3ffed20c959171fa0ebb0ec8
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-04-20 17:33:43 -0300

    Estrutura de pastas base

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod core;
+pub mod display;
+pub mod engine;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("procedure_adventures initial stage!");
+    println!("Definição da estrutura do projeto!");
+
+    /*
+    Ideias para eu poder me guiar e criar a base do projeto
+
+    1. src/core/ (Definições de Objetos)
+        O que vai dentro: O enum Tile, a struct Player e a struct Map.
+        Por que: Aqui fica a "alma" do jogo, apenas os dados e as regras básicas, sem saber como eles aparecem na tela.
+
+    2. src/engine/ (Movimentação e Regras)
+        O que vai dentro: A lógica que decide se o jogador pode se mover (colisão) e o cálculo das novas coordenadas.
+        Por que: Separa o que o jogador é do como ele se comporta.
+
+    3. src/display/ (Impressão na tela)
+        O que vai dentro: Funções que recebem o Map e o Player, dão o clear no terminal e imprimem os caracteres.
+        Por que: Se um dia você decidir sair do terminal e usar uma biblioteca gráfica, você só precisará mexer nessa pasta.
+     */
 }
